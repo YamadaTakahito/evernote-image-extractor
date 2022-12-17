15770d9c06abb4e589ee6726dfd6e45bf323549a
YamadaTakahito <d.p.t.a.d.fighter@gmail.com>
2022-12-17 09:35:51 +0900

    add gitignore

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from evernote.api.client import EvernoteClient
+from evernote.edam.notestore.ttypes import NoteFilter, NotesMetadataResultSpec
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+client = EvernoteClient(
+    token='YOUR_AUTH_ACCESS_TOKEN',
+    sandbox=False
+)
 
+store = client.get_note_store()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+notebook_list = store.listNotebooks()
